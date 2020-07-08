57ca17f9c2665b6f5ff296c1a6b74303048797cc
Alex <alexrandles0@gmail.com>
2020-07-08 15:30:36 +0100

    adding relational database and SQL query which created it

---
+++ create_table.sql
@@ -0,0 +1,16 @@
+CREATE TABLE People (
+    ID int,
+    Name varchar(255),
+    DOB varchar(255),
+    Email varchar(255)
+);  
+
+
+
+INSERT INTO People
+  ( ID, Name, DOB, Email )
+VALUES
+  (0, 'alex' , '20/11/1997', 'alex@gmail.com'), 
+  (1, 'bob', '22/10/1995', 'bob@gmail.com'), 
+  (2, 'rob', '10/02/1990', 'rob@gmail.com'),
+  (3, 'richard', '15/10/1993', 'richard@gmail.com');
