52489bbf4d8fbb24e1f1b0b021d4bd41c5c5971f
Alex <alexrandles0@gmail.com>
2020-10-28 12:52:39 +0000

    updating paper demo

--- create_table.sql
+++ create_table.sql
@@ -1,16 +1,17 @@
 CREATE TABLE People (
     ID int,
-    Name varchar(255),
+    NAME varchar(255),
+    AGE int,
     DOB varchar(255),
-    Email varchar(255)
+    EMAIL varchar(255)
 );  
 
 
 
 INSERT INTO People
-  ( ID, Name, DOB, Email )
+  ( ID, NAME, AGE, DOB, EMAIL )
 VALUES
-  (0, 'alex' , '20/11/1997', 'alex@gmail.com'), 
-  (1, 'bob', '22/10/1995', 'bob@gmail.com'), 
-  (2, 'rob', '10/02/1990', 'rob@gmail.com'),
-  (3, 'richard', '15/10/1993', 'richard@gmail.com');
+  (0, "Alex", 22, "25/11/97", "alex@gmail.com"), 
+  (1, "Kyle", 23, "31/05/97", "kyle@gmail.com"), 
+  (2, "Colm", 23, "04/06/97", "colm@gmail.com");
+
